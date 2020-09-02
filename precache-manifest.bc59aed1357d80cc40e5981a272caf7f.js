6eb059d62d7ed3d5e3c41a49c7c68b51ce473227
Tal-Bar <tal.broshim@gmail.com>
2020-09-02 18:00:55 +0300

    Updates

---
+++ precache-manifest.bc59aed1357d80cc40e5981a272caf7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898303fd63e329edef4c29de52d74b77",
+    "url": "/cat-app/index.html"
+  },
+  {
+    "revision": "b28868232d3ac1e12fcc",
+    "url": "/cat-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "280835b7de6d3907e825",
+    "url": "/cat-app/static/css/main.7182d124.chunk.css"
+  },
+  {
+    "revision": "b28868232d3ac1e12fcc",
+    "url": "/cat-app/static/js/2.47f90d1a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cat-app/static/js/2.47f90d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280835b7de6d3907e825",
+    "url": "/cat-app/static/js/main.b5b40ee8.chunk.js"
+  },
+  {
+    "revision": "c6b5f44b8cf4028d7486",
+    "url": "/cat-app/static/js/runtime-main.1abbdfe9.js"
+  },
+  {
+    "revision": "5d8f6d56cda154af18d4a2bdd8b5839a",
+    "url": "/cat-app/static/media/cat10.5d8f6d56.jpg"
+  }
+]);
